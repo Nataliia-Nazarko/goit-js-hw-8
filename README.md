2c16ce4851633fdf76817500e2b96977cd2978e9
Nataliia Nazarko <nataliianazarko@gmail.com>
2022-01-28 20:22:53 +0200

    task-1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # goit-js-hw-8
+
+Home Work 8
+
+Модуль 8. NPM. Webpack. JSON. Веб-хранилище.
---
+++ src/js/01-gallery.js
@@ -0,0 +1,38 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+    
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);   
+    
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                <div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                            <img class="gallery__image" 
+                                src="${preview}" 
+                                alt="${description}"
+                            />
+                        </a>
+                </div >
+                `;
+            })
+        .join('');
+}
+
+          
+let modalGallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,    
+}); 
+
