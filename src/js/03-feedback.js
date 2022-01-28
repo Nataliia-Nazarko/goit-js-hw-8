20826ff14778a70dd9396c580b87dc3ae6f422af
Nataliia Nazarko <nataliianazarko@gmail.com>
2022-01-28 23:59:56 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
 
+const KEY = 'feedback-form-state';
+
+const email = document.querySelector('input[name=email]');
+const message = document.querySelector('textarea[name=message]')
+
+const input = {email, message};
+    
+input.insertAdjacentHTML('input', newInput);   
+    
+function newInput({ email, message }) {
+    const key = localStorage.getItem(KEY);
+
+    if (key) {
+    player.setCurrentTime(parseFloat(key));
+    }
+ }
