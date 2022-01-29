169bc7ec96e9c0e2ec3150f5477fd1014ba1c2d6
Nataliia Nazarko <nataliianazarko@gmail.com>
2022-01-29 22:27:33 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,17 +2,42 @@ import throttle from 'lodash.throttle';
 
 const KEY = 'feedback-form-state';
 
-const email = document.querySelector('input[name=email]');
-const message = document.querySelector('textarea[name=message]')
+const input = {};
 
-const input = {email, message};
+const inputForm = document.querySelector('.feedback-form');
+ 
+inputForm.addEventListener('input', throttle(newInput, 500));   
     
-input.insertAdjacentHTML('input', newInput);   
-    
-function newInput({ email, message }) {
-    const key = localStorage.getItem(KEY);
+function newInput(event) {
+    input.email = inputForm.elements.email.value;
+    input.message = inputForm.elements.message.value;
+    localStorage.setItem(KEY, JSON.stringify(input));
+}
+ 
+updatePage();
 
-    if (key) {
-    player.setCurrentTime(parseFloat(key));
+function updatePage() {
+    
+    const savedData = localStorage.getItem(KEY);
+
+    if (savedData) {
+        const { email, message } = JSON.parse(savedData);
+        inputForm.email.value = email;
+        inputForm.message.value = message;
     }
- }
+}
+
+inputForm.addEventListener('submit', onFormSubmit);  
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+    console.log({
+        email: inputForm.email.value,
+        message: inputForm.message.value
+    });
+
+    event.currentTarget.reset();
+  localStorage.removeItem(KEY);
+}
+
